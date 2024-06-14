6115d53d2ee0362464b73a7662ad09c2a8004c45
liuye <csu1704liuye@163.com>
2024-06-14 20:43:04 +0800

    Update chat file using boost

--- IOExample/boost_io_example.cpp
+++ IOExample/boost_io_example.cpp
@@ -6,7 +6,7 @@ using boost::asio::ip::tcp;
 void session(tcp::socket& sock) {
     try {
         // 发送消息
-        std::string message = "Hello, Server!";
+        std::string message = "Hello, restaurant! I want the Beef Burgundy!";
         boost::asio::write(sock, boost::asio::buffer(message));
 
         // 接收响应
@@ -21,8 +21,11 @@ void session(tcp::socket& sock) {
 int main() {
     try {
         constexpr int port_num = 8080;
+        // boost::asio::io_context 是处理异步I/O操作的基础
+        // 所有的异步操作最终都是由 io_context 实例来驱动和协调的
         boost::asio::io_context io_context;
 
+        // 专门用于执行TCP网络地址解析
         tcp::resolver resolver(io_context);
         const auto endpoints = resolver.resolve("127.0.0.1", std::to_string(port_num));
         tcp::socket socket(io_context);
